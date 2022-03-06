a75d6d42efa8ba7ff2f62c321496c9f6577e8d06
xrideky <xrideky@stuba.sk>
2022-03-06 17:13:45 +0100

    zaklad dedenia

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -1,10 +1,14 @@
 package sk.stuba.fei.uim.oop;
 
 import sk.stuba.fei.uim.oop.auto.Auto;
+import sk.stuba.fei.uim.oop.auto.Vozidlo;
 
 public class Main {
     public static void main(String[] args) {
-        Auto auto=new Auto();
-        auto.nacitaj();
+        Vozidlo bicykel=new Vozidlo("SOBI 20",2);
+        Auto volvo=new Auto("Volvo","XC90",4);
+
+        bicykel.trubit();
+        volvo.trubit();
     }
 }
--- src/main/java/sk/stuba/fei/uim/oop/auto/Auto.java
+++ src/main/java/sk/stuba/fei/uim/oop/auto/Auto.java
@@ -1,23 +1,14 @@
 package sk.stuba.fei.uim.oop.auto;
 
-import java.util.Random;
-import java.util.Scanner;
-
-public class Auto {
-    private Scanner scanner;
-    private Random random;
-
-    public Auto(){
-        this.scanner=new Scanner(System.in);
-        this.random=new Random();
-
+public class Auto extends Vozidlo {
+    private String modeloveMeno;
+    public Auto(String značka, String modeloveMeno, int pocetKolies) {
+        super(značka, pocetKolies);
     }
 
-    public void nacitaj(){
-        System.out.println("Zadaj string");
-        String meno=scanner.nextLine();
-        System.out.println("Nacital som "+meno);
-
-        System.out.println("Vygeneroval som nahodne cislo od 0 po 1000: "+random.nextInt(1001));
+    @Override
+    public void trubit() {
+        System.out.println("tuuuuuuuuu");
     }
+
 }
