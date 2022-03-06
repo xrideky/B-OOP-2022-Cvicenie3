de85269a1cec70c6cf6c1f50c64ccc18d98b3eb5
xrideky <xrideky@stuba.sk>
2022-03-06 16:58:38 +0100

    Scanner+random, z klavesnice, keyboardInput

--- src/main/java/sk/stuba/fei/uim/oop/auto/Auto.java
+++ src/main/java/sk/stuba/fei/uim/oop/auto/Auto.java
@@ -1,68 +1,23 @@
 package sk.stuba.fei.uim.oop.auto;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Auto {
-   public static final double PALIVO_NA_KM=0.2;
-   public static final double KAPACITA_NADRZE=100;
-   private double stavNadrze;
-   private double kapacitaNadrze;
-   private boolean ojazdene;
+    private Scanner scanner;
+    private Random random;
 
-   //konštruktor
-   public Auto(){
-     this(KAPACITA_NADRZE);
-   }
+    public Auto(){
+        this.scanner=new Scanner(System.in);
+        this.random=new Random();
 
-   //konštruktor so stupným argumentom
-    public Auto(double kapacitaNadrze){
-        this.ojazdene=true;
-        this.stavNadrze=50;
-        this.kapacitaNadrze=kapacitaNadrze;
     }
 
+    public void nacitaj(){
+        System.out.println("Zadaj string");
+        String meno=scanner.nextLine();
+        System.out.println("Nacital som "+meno);
 
-   public void jazdi(double vzdialenost){
-       this.ojazdene=true;
-        stavNadrze = stavNadrze-vzdialenost * PALIVO_NA_KM; //na 1km bude spotreba 0.2 paliva
-        if(stavNadrze<0){
-            stavNadrze=0;
-        }
-    }
-
-    public String stav(){
-       String vysledok;
-       if(this.ojazdene){
-           vysledok="Auto je ojazdene";
-       }else{
-           vysledok="Auto je neojazdene";
-       }
-       vysledok+=" * stav nadrze: "+ stavNadrze +"/"+ kapacitaNadrze;
-       return vysledok;
-    }
-
-    //pristup cez gettery a settery
-    //specialne funkcie ktore ked zavolam bud mi vratia tu hodnotu alebo ju nastavuju
-
-    public double getStavNadrze(){
-       return stavNadrze;
-    }
-
-    public void setStavNadrze(double stavNadrze){
-       this.stavNadrze=stavNadrze;
-    }
-
-    public double getKapacitaNadrze() {
-        return kapacitaNadrze;
-    }
-
-    public void setKapacitaNadrze(double kapacitaNadrze) {
-        this.kapacitaNadrze = kapacitaNadrze;
-    }
-
-    public boolean isOjazdene() {
-        return ojazdene;
-    }
-
-    public void setOjazdene(boolean ojazdene) {
-        this.ojazdene = ojazdene;
+        System.out.println("Vygeneroval som nahodne cislo od 0 po 1000: "+random.nextInt(1001));
     }
 }
