1f1b87d6fcc3da0b90d053b521c2d7fd39adebe1
xrideky <xrideky@stuba.sk>
2022-03-06 15:07:16 +0100

    triedy a modifikatory

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -1,7 +1,21 @@
 package sk.stuba.fei.uim.oop;
 
+import sk.stuba.fei.uim.oop.auto.Auto;
+
 public class Main {
     public static void main(String[] args) {
+        Auto skoda=new Auto();
+        Auto bmw= new Auto();
+
+        skoda.jedna=5;
+        bmw.jedna=7;
+
+        if(bmw.jedna==skoda.jedna){
+            System.out.println("rovnake");
+        }
+        else {
+            System.out.println("rôzne");
+        }
 
     }
 }
---
+++ src/main/java/sk/stuba/fei/uim/oop/auto/Auto.java
@@ -0,0 +1,13 @@
+package sk.stuba.fei.uim.oop.auto;
+
+public class Auto {
+    int pocetKolies;
+    int pocetDveri;
+    String nazov;
+
+    //modifikatory pristupu
+    public int jedna;
+    int dva;
+    protected int tri;
+    private int styri;
+}
